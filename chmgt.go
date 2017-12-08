3d9aaed641da3dcc1ec6a3116c792a5033916d51
Matt Whitney <mattjwhitney@gmail.com>
2017-12-08 07:37:39 -0500

    Created sample structure for routing and handling
    -   Routing is done through the routing directory/package
    -   Handlers are in the handling directory/package
        -   One per file and the file name should match the function name

--- chmgt.go
+++ chmgt.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/mattjw79/chmgt/routing"
+)
+
+func main() {
+	router := routing.NewRouter()
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
---
+++ routing/routes.go
@@ -0,0 +1,39 @@
+package routing
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"github.com/mattjw79/chmgt/handling"
+)
+
+type Route struct {
+	Name    string
+	Method  string
+	Pattern string
+	Handler http.Handler
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		alice.New().ThenFunc(handling.IndexHandler),
+	},
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.Handler)
+	}
+	return router
+}
