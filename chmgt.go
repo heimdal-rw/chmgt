a3288db0985159abe1f35a5b88ce579fda79b786
Matt Whitney <mattjwhitney@gmail.com>
2017-12-09 09:13:53 -0500

    Added logging to show the start of the server

--- chmgt.go
+++ chmgt.go
@@ -9,5 +9,9 @@ import (
 
 func main() {
 	router := routing.NewRouter()
+	// Let the user know that we're starting and on what
+	// port we're listening
+	log.Println("Starting server")
+	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
