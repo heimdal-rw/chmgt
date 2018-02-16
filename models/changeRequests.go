8ab22fcace8d47b2f9afff43599c6e6afb4c0ed1
Matt W <mattjwhitney@gmail.com>
2018-02-16 07:02:28 -0500

    fixed typos

--- models/changeRequests.go
+++ models/changeRequests.go
@@ -6,14 +6,14 @@ func (d *Datasource) GetChangeRequests(id string) ([]Item, error) {
 	return d.GetItems(id, CollectionChangeRequests)
 }
 
-func (d *Datasource) InsertChangeRequest(user Item) error {
-	return d.InsertItem(user, CollectionChangeRequests)
+func (d *Datasource) InsertChangeRequest(cr Item) error {
+	return d.InsertItem(cr, CollectionChangeRequests)
 }
 
-func (d *Datasource) RemoveChangeRequest(user Item) error {
-	return d.RemoveItem(user, CollectionChangeRequests)
+func (d *Datasource) RemoveChangeRequest(cr Item) error {
+	return d.RemoveItem(cr, CollectionChangeRequests)
 }
 
-func (d *Datasource) UpdateChangeRequest(user Item) error {
-	return d.UpdateItem(user, CollectionChangeRequests)
+func (d *Datasource) UpdateChangeRequest(cr Item) error {
+	return d.UpdateItem(cr, CollectionChangeRequests)
 }
